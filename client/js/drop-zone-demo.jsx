9ae4ce91c000752a98a7de79047035bef7dc3cbc
Marc Altmann <mail@marcaltmann.com>
2016-06-30 12:46:35 +0200

    Show preview from react-dropzone component

--- client/js/drop-zone-demo.jsx
+++ client/js/drop-zone-demo.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 
 export default React.createClass({
-  onDrop: function(files) {
-    console.log('Received files: ', files);
+  getInitialState: function () {
+      return {
+        files: []
+      };
   },
+
+  onDrop: function (files) {
+    this.setState({
+      files: files
+    });
+  },
+
   render: function() {
     return (
       <div>
@@ -17,6 +26,10 @@ export default React.createClass({
         >
           <div>Try dropping some files here, or click to select files to upload.</div>
         </Dropzone>
+        {this.state.files.length > 0 ? <div>
+        <h2>Uploading {this.state.files.length} files...</h2>
+        <div>{this.state.files.map((file) => <img src={file.preview} style={{ width: '150px' }} /> )}</div>
+        </div> : null}
       </div>
     );
   }
