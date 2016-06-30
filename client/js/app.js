81c998b0c54b0de209bebec72ef160e613aca339
Marc Altmann <mail@marcaltmann.com>
2016-06-30 12:58:27 +0200

    Test normal image uploader with preview

--- client/js/app.js
+++ client/js/app.js
@@ -5,10 +5,12 @@ import ReactDOM from 'react-dom';
 import PledgeForm from './pledge-form.jsx';
 import SocialMedia from './social-media.jsx';
 import DropzoneDemo from './drop-zone-demo.jsx';
+import ImageUpload from './image-upload.jsx';
 
 const el_app = document.getElementById('app');
 const el_social = document.getElementById('socialmedia');
 const el_drop_zone = document.getElementById('drop-zone');
+const el_image_upload = document.getElementById('image-upload');
 
 if (el_app) {
   ReactDOM.render(
@@ -28,5 +30,12 @@ if (el_drop_zone) {
   ReactDOM.render(
     <DropzoneDemo />,
     el_drop_zone
+  );
+}
+
+if (el_image_upload) {
+  ReactDOM.render(
+    <ImageUpload />,
+    el_image_upload
   );
 }
